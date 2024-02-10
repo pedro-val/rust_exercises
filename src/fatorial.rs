7945bfc24b1b33322a31ef5a58419ba1f963d730
devpedroval <prmval@gmail.com>
2024-02-10 20:41:33 -0300

    Refactor convert_to_int function to return a Result type

--- src/fatorial.rs
+++ src/fatorial.rs
@@ -1,22 +1,41 @@
 use std::io;
 
-fn convert_to_int(input: String) -> i32 {
-    let input: i32 = input.trim().parse::<i32>().unwrap();
-    return input;
+// fn convert_to_int(input: String) -> i32 {
+//     let input: i32 = input.trim().parse::<i32>().unwrap();
+
+//     return input;
+// }
+
+fn convert_to_int(input: String) -> Result<i32, &'static str> {
+    match input.trim().parse::<i32>() {
+        Ok(val) => Ok(val),
+        Err(_) => Err("A entrada fornecida não é um número válido"),
+    }
 }
 
 fn main() {
-    println!("Insira o número positivo para calcular o fatorial: ");
-    let mut number: String = String::new();
-    io::stdin().read_line( &mut number).expect("error");
-    let number: i32 = convert_to_int(number);
-    if number <= 0 {
-        println!("Número inválido");
-    } else {
-        let mut result: i32 = 1;
-        for i in 1..number+1 {
-            result *= i;
+    loop {
+        println!("Insira o número positivo para calcular o fatorial: ");
+        let mut number: String = String::new();
+        io::stdin().read_line(&mut number).expect("Erro ao ler a linha");
+
+        match convert_to_int(number) {
+            Ok(val) => {
+                if val <= 0 {
+                    println!("Número inválido");
+                } else {
+                    let mut result: i32 = 1;
+                    for i in 1..val+1 {
+                        result *= i;
+                    }
+                    println!("Fatorial de {} é: {}", val, result);
+                    break;
+                }
+            },
+            Err(err) => {
+                println!("Erro: {}", err);
+                continue;
+            },
         }
-        println!("Fatorial de {} é: {}", number, result);
     }
 }
